f6e13781f4cec0da43a051a2e219e38c90b0d204
santa <santhosh.003z@gmail.com>
2024-06-17 01:01:25 +0530

    add booking page and style

--- src/pages/booking.js
+++ src/pages/booking.js
@@ -3,7 +3,7 @@ export function booking(content) {
     const bookingHeading = document.createElement(`div`);
     const bookingItems = document.createElement(`div`);
     const bookingForm = document.createElement(`div`);
-    const bookingImg = document.createElement(`img`);
+    const bookingImg = document.createElement(`div`);
 
     bookingContainer.classList.add(`booking-container`);
     bookingHeading .classList.add(`booking-heading`);
@@ -16,5 +16,37 @@ export function booking(content) {
     bookingItems.append(bookingForm, bookingImg);
 
     bookingHeading.textContent = `BOOKING`;
+    bookingImg.src = `../assets/design/about.png`;
+
+    bookingForm.innerHTML = `
+        <form action="">
+            <div class="form-container">
+                <label for="name">Full name</label>
+                <input type="text" id="name">
+            </div>
+            <div class="form-container">
+                <label for="time">Time</label>
+                <input type="text" id="time">
+            </div>
+            <div class="form-container">
+                <label for="phone">Phone number</label>
+                <input type="tel" id="phone">
+            </div>
+            <div class="form-container">
+                <label for="guest">Guest</label>
+                <input type="number" id="guest">
+            </div>
+            <div class="form-container">
+                <label for="date">Date</label>
+                <input type="date" id="date">
+            </div>
+            <div class="form-container">
+                <input type="submit" value="RESERVE" id="reserve">
+            </div>      
+        </form>`;
     
+    const reserve = document.querySelector(`#reserve`);
+    reserve.addEventListener(`click`, ()=>{
+        event.preventDefault();
+    })
 };
